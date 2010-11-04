fa0583a6ce39a5649e0a7dec26b66b9d756c2798
Chris Aniszczyk <caniszczyk@gmail.com>
2010-11-04 04:28:42 -0500

    Initial Import
    Signed-off-by: Chris Aniszczyk <caniszczyk@gmail.com>

---
+++ org.aniszczyk.minerva.tests.core/src/org/aniszczyk/minerva/tests/core/SimpleTest.java
@@ -0,0 +1,5 @@
+package org.aniszczyk.minerva.tests.core;
+
+public class SimpleTest {
+
+}
---
+++ org.aniszczyk.minerva.tests.ui/src/org/aniszczyk/minerva/tests/ui/SimpleUITestCase.java
@@ -0,0 +1,37 @@
+/*******************************************************************************
+ * Copyright (C) 2010, Chris Aniszczyk <caniszczyk@gmail.com>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.aniszczyk.minerva.tests.ui;
+
+import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
+import org.eclipse.swtbot.swt.finder.exceptions.WidgetNotFoundException;
+import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
+import org.junit.After;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+
+@RunWith(SWTBotJunit4ClassRunner.class)
+public abstract class SimpleUITestCase {
+
+	protected static final SWTWorkbenchBot bot = new SWTWorkbenchBot();
+
+	@BeforeClass
+	public static void closeWelcomePage() {
+		try {
+			bot.viewByTitle("Welcome").close();
+		} catch (WidgetNotFoundException e) {
+			// somebody else probably closed it, lets not feel bad about it.
+		}
+	}
+
+	@After
+	public void clickButton() {
+		// TODO
+	}
+
+}
