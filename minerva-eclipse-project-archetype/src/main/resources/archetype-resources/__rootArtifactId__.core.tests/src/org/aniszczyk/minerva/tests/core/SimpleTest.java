6aa8c86a3ac0222fd0c49712d3acdc15a627be2a
David Carver <dcarver@carver-intalio.(none)>
2011-04-18 12:19:13 -0400

    Adding a basic maven archetype to generate a simple eclipse project that uses Tycho 0.11.  See README file for usage information and deployment.

---
+++ minerva-eclipse-project-archetype/src/main/resources/archetype-resources/__rootArtifactId__.core.tests/src/org/aniszczyk/minerva/tests/core/SimpleTest.java
@@ -0,0 +1,30 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+/*******************************************************************************
+ * Copyright (C) 2011, Chris Aniszczyk <caniszczyk@gmail.com>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package ${package}.tests.core;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import ${package}.core.Model;
+import org.junit.Test;
+
+public class SimpleTest {
+	
+	@Test
+	public void simpleTest() {
+		Model model = new Model();
+		assertNull(model.getId());
+		model.setId("id");
+		assertEquals("id", model.getId());
+	}
+
+}
